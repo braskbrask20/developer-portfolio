fe0ac8500cd5127947446c725b9239d384322125
widstacks <braskbrask20@gmail.com>
2026-07-20 06:09:08 -0400

    (practice JavaScript operators and conditional statements

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ console.log("stacks");
 console.log("32");
 console.log("software engineer");
 console.log("java script");
+let age = 10;
+if (age >= 10) {
+    console.log("Eligible");
+} else {
+    console.log("Ineligible");
+}
