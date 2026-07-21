af47796186c3b3738d9d70254632f103c0c15235
widstacks <braskbrask20@gmail.com>
2026-07-21 07:25:25 -0400

    Learn JavaScript functions and reusable code

--- js/script.js
+++ js/script.js
@@ -9,3 +9,41 @@ if (age >= 10) {
 } else {
     console.log("Ineligible");
 }
+
+// Welcome Function
+function welcome() {
+console.log("Welcome to my portfolio!");
+}
+
+welcome();
+
+
+// Greeting Function
+function greet(name) {
+console.log("Hello, " + name);
+}
+
+greet("Stacks");
+
+
+// Addition Function
+function add(num1, num2) {
+return num1 + num2;
+}
+
+console.log(add(5, 3));
+
+
+// Age Checker
+function checkAge(age) {
+
+if (age >= 18) {
+return "Adult";
+} else {
+return "Minor";
+}
+
+}
+
+console.log(checkAge(20));
+console.log(checkAge(15));
